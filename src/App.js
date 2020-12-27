e7b575e26d2aace7046d89c81b78b11dd7523459
coolkim99 <coolkim2016@gmail.com>
2020-12-28 02:35:03 +0900

    movie_app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import Potato from "./Potato";
-
+import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Detail from "./routes/Detail";
+import Navigation from "./components/Navigation";
+import "./App.css";
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
-      <Potato />
-    </div>
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/movie/:id" component={Detail} />
+    </HashRouter>
   );
 }
-
 export default App;
